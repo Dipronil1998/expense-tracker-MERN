0069537583a999b9dc5f6bc509cc83277b325990
dipronil <dipronil.das@techwens.in>
2023-09-14 19:22:27 +0530

    prompt added

--- client/src/components/Expense.js
+++ client/src/components/Expense.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import moment from 'moment';
 import '../assets/css/Expense.css';
+import { Button, message, Popconfirm } from 'antd';
 
 const Expense = ({ expenseData }) => {
+  const confirm = (e) => {
+    console.log(e);
+    message.success('Click on Yes');
+  };
+  const cancel = (e) => {
+    console.log(e);
+    message.error('Your Data is saved.');
+  };
   return (
     <div className="parent-container">
       <div className="expense-card">
@@ -10,7 +19,17 @@ const Expense = ({ expenseData }) => {
           <h3>{expenseData.title}</h3>
           <div className="expense-buttons">
             <button className="edit-button">Edit</button>
-            <button className="delete-button">Delete</button>
+            <Popconfirm
+              title="Delete the task"
+              description="Are you sure to delete this task?"
+              onConfirm={confirm}
+              onCancel={cancel}
+              okText="Yes"
+              cancelText="No"
+            >
+              <Button className="delete-button">Delete</Button>
+            </Popconfirm>
+            {/* <button className="delete-button">Delete</button> */}
           </div>
         </div>
         <p className="expense-details">
