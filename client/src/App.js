3fc4662189bddd0ed1e39f22ac5a94fa2ddc72a7
Dipronil1998 <dipronilda.net@gmail.com>
2024-02-24 20:50:27 +0530

    1st day of month site is not open

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import { Container, Row } from 'react-bootstrap';
 import './App.css';
 import { AddExpenseButton, AuthModal, ExpenseCards, ExpenseList } from "./components"
 import { useAppContext } from './context/appContext';
+import UnderConstructionMessage from './components/UnderConstructionMessage';
 
 function App() {
   const { authModal, authorized } = useAppContext();
   const [isModalOpen, setIsModalOpen] = useState(authModal);
+  const [isOpenSite, setIsOpenSite] = useState(true);
+
+  useEffect(() => {
+    const currentDate = new Date();
+
+    if (currentDate.getDate() === 1) {
+      console.log('Site is not open on the 1st of the month');
+      setIsOpenSite(false)
+    } else {
+      console.log('Site is open');
+      setIsOpenSite(true)
+    }
+  }, [isOpenSite]);
 
   return (
     <>
+    {isOpenSite? (<>
       {authModal && <AuthModal open={isModalOpen} />}
       {!authModal && (
         <>
@@ -22,6 +37,7 @@ function App() {
           <ExpenseList />
         </>
       )}
+    </>) :(<UnderConstructionMessage/>)}
     </>
   );
 }
---
+++ client/src/components/UnderConstructionMessage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../assets/css/UnderConstructionMessage.css';
+
+const UnderConstructionMessage = () => {
+  return (
+    <div className="under-construction-container">
+      <h1>Site Under Construction</h1>
+      <p>We are working on something awesome. Please check back later!</p>
+    </div>
+  );
+};
+
+export default UnderConstructionMessage;
